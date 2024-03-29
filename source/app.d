2389947580ddce9ef6971a299ba7a743830f2877
Hazel Atkinson <yellowsink@riseup.net>
2024-03-29 17:38:07 +0000

    first prototype impl of a reactor!

--- source/app.d
+++ source/app.d
@@ -5,75 +5,10 @@ import core.thread.osthread : Thread;
 import core.atomic : atomicOp;
 import std.datetime : dur;
 
-// NOTE:
-// this is all just messing around and playing here
-// none of this is rockhopper code.
-
-void naiveSleep(ulong d)
-{
-  Thread.sleep(dur!"msecs"(d));
-}
-
-void schedulerSleep(ulong d)
-{
-  auto cond = scheduler.newCondition(null);
-  cond.wait(dur!"msecs"(d));
-}
-
-void threadWaitSleep(ulong d)
-{
-  shared int done = 0;
-  new Thread({
-    Thread.sleep(dur!"msecs"(d));
-    atomicOp!"+="(done, 1);
-  }).start();
-
-  auto cond = scheduler.newCondition(null);
-  while (!done)
-    cond.wait(dur!"msecs"(2)); // we'll just poll 2ms for now
-}
-
 void main()
 {
-  // set up the scheduler, in this case, one backed by fibers
-  scheduler = new FiberScheduler;
+  import reactor : reactor;
 
-  /* scheduler.spawn({
-    for (int i = 0; i < 10; i++) {
-      writeln("hi!");
-      schedulerSleep(100);
-    }
-  }); */
-  /* scheduler.start({
-    writeln("hi");
-    threadWaitSleep(500);
-    writeln("wow!");
-    schedulerSleep(500);
-    writeln("sched.");
-  }); */
+  writeln(reactor);
 
-scheduler.start({
-  void queueTask(int x) {
-    scheduler.spawn({ writeln("hi from ", x); schedulerSleep(1_000); writeln("bye from ", x); });
-  }
-
-  // start a few tasks on the scheduler
-  for (auto i = 0; i < 5; i++)
-    queueTask(i);
-});
-//  scheduler.start({});
-
-/* scheduler = new FiberScheduler;
-
-// run twice in parallel
-scheduler.spawn({
-  writeln("hi 1!");
-  scheduler.newCondition(null).wait(dur!"msecs"(250));
-  writeln("bye 1");
-});
-scheduler.start({
-  writeln("hi 2!");
-  scheduler.newCondition(null).wait(dur!"msecs"(250));
-  writeln("bye 2");
-}); */
 }
--- source/reactor.d
+++ source/reactor.d
@@ -1,15 +1,128 @@
-private
+import std.typecons : Nullable;
+
+private Nullable!Reactor _reactorBacking;
+
+// Returns the reactor for the current thread.
+public Reactor reactor() @property // @suppress(dscanner.confusing.function_attributes)
 {
-  bool isSetup = false;
+  if (_reactorBacking.isNull)
+    _reactorBacking = new Reactor;
 
-  Fiber systemFiber;
+  return _reactorBacking.get;
+}
 
-  void ensureSetup()
+
+/*private*/ class Reactor
+{
+  import std.typecons : Tuple, tuple;
+  import core.thread.fiber : Fiber;
+  import taggedalgebraic : TaggedUnion;
+  import eventcore.core : eventDriver;
+  import eventcore.driver : EventID, FileFD, PipeFD, IOMode, ProcessID, TimerID, ExitReason;
+  import std.datetime : Duration;
+
+  union _FiberBlockerRaw
   {
-    if (isSetup) return;
-    isSetup = true;
+    // TODO: support not-timers
+    //string nsLookup;
+    //EventID ecThreadEvent;
+    //Tuple!(FileFD, ulong, ubyte[], IOMode) fileRead;
+    //Tuple!(FileFD, ulong, const(ubyte)[], IOMode) fileWrite;
+    //Tuple!(PipeFD, ulong, ubyte[], IOMode) pipeRead;
+    //Tuple!(PipeFD, ulong, const(ubyte)[], IOMode) pipeWrite;
+    //ProcessID procWait;
+    //int signalTrap;
+    // TODO: sockets
+    TimerID sleep;
+    // TODO: directory watchers
+  }
+  alias FiberBlocker = TaggedUnion!_FiberBlockerRaw;
 
-    scheduler = new FiberScheduler;
+  class WrappedFiber
+  {
+    // the actual fiber
+    Fiber fiber;
+    // when unset, fiber is unblocked, when set, the blocker the fiber is waiting on
+    Nullable!FiberBlocker currentBlocker;
+    // if the current blocker has had its callback registered or not
+    bool blockerRegistered;
+    // when set, the result of the blocker (file data, etc) to be passed back to the fiber
+    // TODO: support more than timers
+    Nullable!TimerID blockerResult;
+  }
 
+  // The currently executing fiber.
+  Nullable!WrappedFiber _currentFiber;
+  public inout(Nullable!WrappedFiber) currentFiber() @property inout
+  {
+    return _currentFiber;
+  }
+
+  WrappedFiber[] fibers;
+
+  //this(){}
+
+  void enqueueFiber(WrappedFiber f)
+  {
+    fibers ~= f;
+  }
+
+  /*void setBlockerAndYield(FiberBlocker b)
+  {
+    // call site: within a fiber
+    auto f = _currentFiber.get;
+    assert(f.currentBlocker.isNull);
+    f.currentBlocker = b;
+    Fiber.yield();
+  }*/
+
+  void loop()
+  {
+    import std.array : array;
+    import std.algorithm : map, filter;
+
+    while (fibers.length)
+    {
+      // step 1: run all fibers (clone arr first to disambiguate mutation behaviour)
+      foreach(f; fibers.array)
+      {
+        _currentFiber = f;
+        f.fiber.call();
+        _currentFiber.nullify();
+      }
+
+      // step 2: get fibers with blockers
+      auto fibersToRegister = fibers.filter!(f => !f.currentBlocker.isNull && !f.blockerRegistered).array;
+
+      // step 3: register callbacks
+      foreach (f; fibersToRegister)
+      {
+        f.blockerRegistered = true;
+        // TODO: support blockers other than `sleep`
+        auto tid = f.currentBlocker.get.sleepValue;
+
+        eventDriver.timers.wait(tid, (TimerID _timerId) nothrow {
+          assert(tid == _timerId);
+
+          // resolve blocker!
+          f.currentBlocker.nullify();
+          f.blockerRegistered = false;
+          f.blockerResult = tid;
+        });
+      }
+
+      // step 4: run event loop!
+      // when processEvents is called with no params, will wait unless none are queued
+      // instead, we want to just wait indefinitely if there are no queued events, so pass Duration.max
+      auto eventFinishReason = eventDriver.core.processEvents(Duration.max);
+      if (eventFinishReason == ExitReason.exited)
+      {
+        // TODO: does handling a user-requested exit need more finesse?
+        break;
+      }
+      // TODO: what is ExitReason.idle?
+      // ExitReason.outOfWaiters -> impossible due to Duration.max (or waits a super long time)
+      // ExitReason.timeout -> impossible, or, the system has been runnning for Duration.max time and is about to explode
+    }
   }
 }
