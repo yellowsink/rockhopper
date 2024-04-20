f3396feaabe5262a2e012f8ca3451dd5ec2dba64
Hazel Atkinson <yellowsink@riseup.net>
2024-04-20 15:30:52 +0100

    [task] add simple task abstraction

--- source/app.d
+++ source/app.d
@@ -4,6 +4,7 @@ import std.datetime : dur, MonoTime;
 import reactor;
 import llevents;
 import sync;
+import task;
 
 import eventcore.core : eventDriver;
 
@@ -12,41 +13,20 @@ import core.thread.osthread : Thread;
 void main()
 {
 	entrypoint({
-		// test of using a "thread" event on only one thread
-
-		// fiber -> notify other thread
-		auto evF2T = new TEvent;
-		// other thread -> notify main thread fiber
-		// otherwise the main thread exists and kills the other fiber!
-		// god i HATE threads so much this is why rockhopper even exists
-		auto evT2F = new TEvent;
-
-		auto t = new Thread({
-			try {
-			entrypoint({
-				writeln("2: before wait");
-				evF2T.wait(); // this throws?
-
-				writeln("hi");
-
-				evT2F.notify();
-				writeln("2: done");
-			});
-			} catch(Throwable e) { writeln (e);}
-			writeln("2: exited reactor");
-		}).start();
-
-		spawn({
+		auto vtask = tSpawn({
+			writeln("task says hi");
 			sleep(dur!"msecs"(500));
-			writeln("1: before notify");
-			evF2T.notify();
-
-
-			writeln("1: before wait");
-			writeln(t.isRunning);
-			evT2F.wait();
-			sleep(dur!"msecs"(500));
-			writeln("1: done");
+		}).then({
+			writeln("async continuation!");
 		});
+
+		auto itask = tSpawn({
+			writeln("new task here");
+			return 5;
+		}).then((int v) => v > 6); // continuations can modify the value
+
+		vtask.waitRes();
+
+		writeln(itask.waitRes());
 	});
 }
---
+++ source/task.d
@@ -0,0 +1,82 @@
+// `task` provides a very minimal task implementation on top of fibers.
+
+module task;
+
+import reactor : spawn; // used in constructor and in then
+import sync : FEvent; // used for syncing the fibers
+
+import std.typecons : Nullable; // used by tryGetRes
+
+import std.traits : isSomeFunction, Parameters, ReturnType; // used for template inference stuff
+
+struct Task(T)
+{
+	enum VALUED = !is(T == void);
+
+	private FEvent ev;
+
+	static if(VALUED)
+		private T res;
+
+	// may only call this with T function() or T delegate()
+	// we use a separate function to wrap this so we can infer the type T from F.
+	private this(F)(F dg)
+	{
+		static assert(isSomeFunction!F && Parameters!F.length == 0 && is(ReturnType!F == T));
+
+		spawn({
+			static if(VALUED)
+				res = dg();
+			else
+				dg();
+			ev.notify();
+		});
+	}
+
+	bool isFinished() inout @property
+	{
+		return ev.isSignaled;
+	}
+
+	static if(VALUED)
+		alias MAYBERES = Nullable!T;
+	else
+		alias MAYBERES = bool;
+
+	MAYBERES tryGetRes()
+	{
+		static if(!VALUED)
+			return isFinished;
+		else
+			return isFinished ? MAYBERES(res) : MAYBERES.init;
+	}
+
+	T waitRes()
+	{
+		ev.wait();
+		static if(VALUED) return res;
+	}
+
+	auto then(F)(F fn)
+	if (VALUED && isSomeFunction!F && Parameters!F.length == 1 && is(Parameters!F[0] == T))
+	{
+		return Task!(ReturnType!F)({
+			return fn(waitRes());
+		});
+	}
+
+	auto then(F)(F fn)
+	if (!VALUED && isSomeFunction!F && Parameters!F.length == 0)
+	{
+		return Task!(ReturnType!F)({
+			waitRes();
+			fn();
+		});
+	}
+}
+
+auto tSpawn(F)(F fn)
+if (isSomeFunction!F && Parameters!F.length == 0)
+{
+	return Task!(ReturnType!F)(fn);
+}
